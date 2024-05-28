0bcacc4627aee38645b2f5e388b0f7dd30f0b564
gymtrackpage <gillards9900@gmail.com>
2024-05-28 09:29:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -34,3 +34,33 @@ function loadImage(url) {
     img.src = url;
   });
 }
+
+ // Color Matching
+  const pixelData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+  for (let i = 0; i < pixelData.length; i += 4) {
+    const r = pixelData[i];
+    const g = pixelData[i + 1];
+    const b = pixelData[i + 2];
+
+    const closestColor = findClosestColor(r, g, b, colors);
+    ctx.fillStyle = closestColor.Hex;
+    ctx.fillRect(i / 4 % canvas.width, Math.floor(i / 4 / canvas.width), 1, 1);
+  }
+}
+
+function findClosestColor(r, g, b, colors) {
+  let closestColor = null;
+  let smallestDistance = Infinity;
+  for (const color of colors) {
+    const distance = Math.sqrt(
+      Math.pow(r - color.Red, 2) +
+      Math.pow(g - color.Green, 2) +
+      Math.pow(b - color.Blue, 2)
+    );
+    if (distance < smallestDistance) {
+      closestColor = color;
+      smallestDistance = distance;
+    }
+  }
+  return closestColor;
+}
